c0f5c147610566a0be6bb666c5f3ae33f3e5b0f5
Adityaada0 <Adityastd2003@gmail.com>
2023-11-01 22:28:57 +0530

    Eighth commit

---
+++ om8.sql
@@ -0,0 +1,11 @@
+USE clg;
+
+SELECT MIN(salary) AS 'Minimum Salary' FROM employee;
+
+SELECT MAX(salary) AS 'Maximum Salary' FROM employee;
+
+SELECT AVG(salary) AS 'Average Salary' FROM employee;
+
+SELECT COUNT(*) AS 'Number of Employees' FROM employee;
+
+SELECT SUM(salary) AS 'Total Salary' FROM employee;
